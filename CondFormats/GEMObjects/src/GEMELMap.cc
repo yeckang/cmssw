73b142f0b109f40833d9a2692401837f5630f9a0
yeckang <yechan.kang@cern.ch>
2022-04-10 15:14:58 +0200

    update GEM EMaps for GE21 Readout

--- CondFormats/GEMObjects/src/GEMELMap.cc
+++ CondFormats/GEMObjects/src/GEMELMap.cc
@@ -11,18 +11,16 @@ GEMELMap::~GEMELMap() {}
 const std::string& GEMELMap::version() const { return theVersion; }
 
 void GEMELMap::convert(GEMROmap& romap) {
-  for (auto imap : theVFatMap_) {
-    for (unsigned int ix = 0; ix < imap.vfatId.size(); ix++) {
+  for (auto imap : theChamberMap_) {
+    for (unsigned int ix = 0; ix < imap.chamberType.size(); ix++) {
       GEMROmap::eCoord ec;
-      ec.vfatId = imap.vfatId[ix] & chipIdMask_;
       ec.gebId = imap.gebId[ix];
       ec.amcId = imap.amcId[ix];
 
       int st = std::abs(imap.z_direction[ix]);
       GEMROmap::dCoord dc;
-      dc.gemDetId = GEMDetId(imap.z_direction[ix], 1, st, imap.depth[ix], imap.sec[ix], imap.iEta[ix]);
-      dc.vfatType = imap.vfatType[ix];
-      dc.iPhi = imap.iPhi[ix];
+      dc.gemDetId = GEMDetId(imap.z_direction[ix], 1, st, imap.depth[ix], imap.sec[ix], 0);
+      dc.chamberType = imap.chamberType[ix];
 
       romap.add(ec, dc);
       romap.add(dc, ec);
@@ -30,14 +28,16 @@ void GEMELMap::convert(GEMROmap& romap) {
   }
 
   for (auto imap : theStripMap_) {
-    for (unsigned int ix = 0; ix < imap.vfatType.size(); ix++) {
+    for (unsigned int ix = 0; ix < imap.chamberType.size(); ix++) {
       GEMROmap::channelNum cMap;
-      cMap.vfatType = imap.vfatType[ix];
+      cMap.chamberType = imap.chamberType[ix];
+      cMap.vfatAdd = imap.vfatId[ix];
       cMap.chNum = imap.vfatCh[ix];
 
       GEMROmap::stripNum sMap;
-      sMap.vfatType = imap.vfatType[ix];
-      sMap.stNum = imap.vfatStrip[ix];
+      sMap.chamberType = imap.chamberType[ix];
+      sMap.iEta = imap.iEta[ix];
+      sMap.stNum = imap.strip[ix];
 
       romap.add(cMap, sMap);
       romap.add(sMap, cMap);
@@ -58,31 +58,42 @@ void GEMELMap::convertDummy(GEMROmap& romap) {
       if (st == 0)
         maxVFat = maxVFatGE0_;
 
+      uint16_t chipId = 0;
+      for (int roll = 1; roll <= GEMDetId::maxRollId; ++roll) {
+        for (int nphi = 1; nphi <= maxVFat; ++nphi) {
+          for (int i = 0; i < maxChan_; ++i) {
+            // only 1 chamber type for dummy map
+            GEMROmap::channelNum cMap;
+            cMap.chamberType = st;
+            cMap.chNum = i;
+
+            GEMROmap::stripNum sMap;
+            sMap.chamberType = st;
+            sMap.stNum = i + 1;
+
+            romap.add(cMap, sMap);
+            romap.add(sMap, cMap);
+          }
+          chipId++;
+        }
+      }
+
       for (int ch = 1; ch <= GEMDetId::maxChamberId; ++ch) {
         for (int ly = 1; ly <= GEMDetId::maxLayerId; ++ly) {
           // 1 geb per chamber
+          GEMDetId gemId(re, 1, st, ly, ch, 0);
           gebId++;
-          uint16_t chipId = 0;
-          for (int roll = 1; roll <= GEMDetId::maxRollId; ++roll) {
-            GEMDetId gemId(re, 1, st, ly, ch, roll);
 
-            for (int nphi = 1; nphi <= maxVFat; ++nphi) {
-              chipId++;
+          GEMROmap::eCoord ec;
+          ec.gebId = gebId;
+          ec.amcId = amcId;
 
-              GEMROmap::eCoord ec;
-              ec.vfatId = chipId;
-              ec.gebId = gebId;
-              ec.amcId = amcId;
+          GEMROmap::dCoord dc;
+          dc.gemDetId = gemId;
+          dc.chamberType = st;
 
-              GEMROmap::dCoord dc;
-              dc.gemDetId = gemId;
-              dc.vfatType = 1;
-              dc.iPhi = nphi;
-
-              romap.add(ec, dc);
-              romap.add(dc, ec);
-            }
-          }
+          romap.add(ec, dc);
+          romap.add(dc, ec);
           // 5 bits for geb
           if (gebId == maxGEBs_) {
             // 24 gebs per amc
@@ -92,19 +103,5 @@ void GEMELMap::convertDummy(GEMROmap& romap) {
         }
       }
     }
-  }
-
-  for (int i = 0; i < maxChan_; ++i) {
-    // only 1 vfat type for dummy map
-    GEMROmap::channelNum cMap;
-    cMap.vfatType = 1;
-    cMap.chNum = i;
-
-    GEMROmap::stripNum sMap;
-    sMap.vfatType = 1;
-    sMap.stNum = i + 1;
-
-    romap.add(cMap, sMap);
-    romap.add(sMap, cMap);
   }
 }
