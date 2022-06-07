771a27b884a86f58d369b2d20fa3d72671df5c71
yeckang <yechan.kang@cern.ch>
2022-06-07 16:57:17 +0200

    add script for the inetegration setup

--- EventFilter/GEMRawToDigi/test/runGEMUnpacker_cfg.py
+++ EventFilter/GEMRawToDigi/test/runGEMUnpacker_cfg.py
@@ -144,7 +144,9 @@ from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, '112X_dataRun3_Prompt_v5', '')
 ## for the time being the mapping does not work with the data label. Use MC instead
 if options.useB904Data:
-    process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase1_2021_realistic', '')
+    process.GlobalTag.toGet = cms.VPSet(cms.PSet(connect = cms.string('sqlite_fip:EventFilter/GEMRawToDigi/data/GEMChMap_coffin.db'),
+                                                 record = cms.string('GEMChMapRcd'),
+                                                 tag = cms.string('GEMChMap')))
 
 # dump raw data
 process.dumpRaw = cms.EDAnalyzer(
